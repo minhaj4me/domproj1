71151133e018af11db629b44916b45089f178f45
Minhaj Haider <63444276+minhaj4me@users.noreply.github.com>
2021-08-18 00:07:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var counter=document.querySelector(".counter");
+var followers=document.querySelector(".followers");
+
+// counter.innerHTML=1001;
+
+let count=1;
+
+setInterval( () => {
+    if(count<1000){
+        count++;
+    counter.innerText=count;
+    }
+} , 1);
+
+setTimeout( () => {
+    followers.innerHTML="Followers on Instagram!"
+},4200)
